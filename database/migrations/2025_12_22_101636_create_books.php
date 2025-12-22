9f980cf175a5dcfb6261d576791a68168cfec430
pierre <pierrekhouryp536@gmail.com>
2025-12-22 14:23:16 +0300

    make migration

---
+++ database/migrations/2025_12_22_101636_create_books.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->char('ISBN',13)->primary();
+            $table->string('title',50);
+            $table->decimal('price',4,2);
+            $table->decimal('mortgage',5,2);
+            $table->date('authorship_date')->nullable();
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2025_12_22_105017_create_customers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->enum('gender',['male','female']);
+            $table->char('phone',10)->unique();
+            $table->string('avatar');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
