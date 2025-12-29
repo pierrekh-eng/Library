a85786030a2ad2802e4370706b36858e94b9daf5
pierre <pierrekhouryp536@gmail.com>
2025-12-29 16:50:18 +0300

    seed the book with factory

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasFactory;
+    
     protected $primaryKey = 'ISBN';
     public $incrementing = false;
     protected $fillable = ["ISBN","title","price","mortgage","category_id"];
---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "ISBN"=>$this->faker->isbn13(),
+            "title"=>$this->faker->text(50),
+            "price"=>$this->faker->randomFloat(2,0,99),
+            "mortgage"=>$this->faker->randomFloat(2,0,999),
+            "category_id"=>Category::all()->random()->id
+        ];
+    }
+}
