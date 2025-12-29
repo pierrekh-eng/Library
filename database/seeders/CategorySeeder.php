8a0f2e0b8f5d32ef1927101c228a9d79ffbcf811
pierre <pierrekhouryp536@gmail.com>
2025-12-29 16:39:58 +0300

    seed category and user

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories=[
+            ['name'=>'Laravel'],
+            ['name'=>'Medecine'],
+            ['name'=>'Farming'],
+        ];
+        Category::insert($categories);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user=['name'=>'test' , 'email'=>'test@gmail.com','password'=>'123'];
+        User::create($user);
+    }
+}
